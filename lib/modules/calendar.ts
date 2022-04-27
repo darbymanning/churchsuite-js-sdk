673f5df5810364427131a55cd811a80ad9b70985
Darby Manning <darby@kyan.com>
2022-04-27 23:51:39 +0100

    Correct Calendar endpoint URLs

--- lib/modules/calendar.ts
+++ lib/modules/calendar.ts
@@ -14,48 +14,48 @@ export default function ({
     events: {
       /** List/search events */
       async list() {
-        return await get<Calendar.Events>("v1/calendar/events");
+        return await get<Calendar.Events>("/v1/calendar/events");
       },
       /** Return data for a specific event */
       async show(id) {
-        return await get<Calendar.Event>(`v1/calendar/event/${id}`);
+        return await get<Calendar.Event>(`/v1/calendar/event/${id}`);
       },
       /** Return tickets data for a specific event */
       async tickets(id) {
         return await get<Calendar.EventTickets>(
-          `v1/calendar/events/${id}/tickets`
+          `/v1/calendar/events/${id}/tickets`
         );
       },
       signups: {
         /** Return data regarding people who have signed up for a specific event */
         async list(id) {
           return await get<Calendar.EventSignUps>(
-            `v1/calendar/events/${id}/signups`
+            `/v1/calendar/events/${id}/signups`
           );
         },
         /** Create a new sign up for a specific event */
         async create(id, args) {
           return await post<Calendar.EventSignUpResponse>(
-            `v1/calendar/event/${id}/signups`,
+            `/v1/calendar/event/${id}/signups`,
             args
           );
         },
         /** Update the specified sign up for a specific event */
         async update(eventId, signUpId, args) {
           return await put<Calendar.EventSignUp>(
-            `v1/calendar/event/${eventId}/signup/${signUpId}`,
+            `/v1/calendar/event/${eventId}/signup/${signUpId}`,
             args
           );
         },
         /** Remove the specified sign up for a specific event */
         async del(eventId, signUpId) {
-          return await del(`v1/calendar/event/${eventId}/signup/${signUpId}`);
+          return await del(`/v1/calendar/event/${eventId}/signup/${signUpId}`);
         },
       },
     },
     /** Return categories ordered alphabetically */
     async categories() {
-      return await get<Calendar.Categories>("v1/calendar/categories");
+      return await get<Calendar.Categories>("/v1/calendar/categories");
     },
   };
 }
